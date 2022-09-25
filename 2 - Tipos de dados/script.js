ddd59fc97e2dc210b0a6abdfd385ba660ae420a4
Hugo Leonardo <hugolcm@live.com>
2022-09-25 16:39:38 -0300

    aulas 1, 2 e 3

---
+++ 2 - Tipos de dados/script.js
@@ -0,0 +1,17 @@
+var nome = 'fulano'; //string
+console.log(typeof nome);
+var age = 36; //number
+console.log(typeof age);
+var hasDegree = true; //boolean
+console.log(typeof hasDegree);
+var team; //undefined
+console.log(typeof team);
+var food = null; //null
+console.log(typeof food);
+var symbol = Symbol(); //symbol
+console.log(typeof symbol);
+var newObject = {}; //object
+// console.log(typeof newObject);
+
+console.log('it\'s');
+console.log(`my name is ${nome}`); //template string
---
+++ 3 - Números e operadores/script.js
@@ -0,0 +1,17 @@
+var pi = 3.14
+var exp = 2e10 //qtd de 0 ao lado do 2
+
+var sum = 100 + 50 //150
+var sub = 100 - 50 //50
+var multiplication = 100 * 2 //200
+var division = 100 / 50 //2
+var exponent = 2 ** 4 //16
+var module = 14 % 5 //4
+var sumString = '100' + 50 //10050
+var sumString = '100' - 50 //50
+var multiplicationString = '100' * '2' //200
+var divisionString = 'test' / '2' //Nan
+
+var x = '1'
++x //1 (number)
+-x //-1 (number)
